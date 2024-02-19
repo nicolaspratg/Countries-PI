f17dddc7c4bc37a704e6416315ecfa43404ac053
nicolaspratg <nicoprat@hotmail.com>
2024-02-19 09:49:24 -0300

    added wrapping div

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -4,17 +4,19 @@ import "./Card.css"; // Import CSS file for styling
 
 const Card = ({ country }) => {
   return (
-    <Link to={`/detail/${country.id}`} className="card-link">
-      <div className="card">
-        <div className="card-content">
-          <img src={country.flagImage} alt="Flag" className="card-image" />
-          <div className="card-text">
-            <h2 className="card-title">{country.name}</h2>
-            <p className="card-continent">Continent: {country.continent}</p>
+    <div>
+      <Link to={`/detail/${country.id}`} className="card-link">
+        <div className="card">
+          <div className="card-content">
+            <img src={country.flagImage} alt="Flag" className="card-image" />
+            <div className="card-text">
+              <h2 className="card-title">{country.name}</h2>
+              <p className="card-continent">Continent: {country.continent}</p>
+            </div>
           </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 };
 
