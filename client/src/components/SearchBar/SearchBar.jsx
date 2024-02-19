62c8cdcd098054be73c525886a553db254a62ca8
nicolaspratg <nicoprat@hotmail.com>
2024-02-19 09:46:01 -0300

    onSearch finished

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,13 +1,35 @@
-import "./SearchBar.css"
+import { useState } from "react";
+import { useDispatch } from "react-redux"; // Import useDispatch hook
+import { searchCountry } from "../../redux/actions"; // Import your searchCountry action
+import "./SearchBar.css";
 
-export const SearchBar = () => { 
-    return (
-        <div>
-            <div><input type="search" name="" id="" placeholder="Search Country" /></div>
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
+export const SearchBar = () => {
+  const dispatch = useDispatch(); // Get the dispatch function
+  const [searchTerm, setSearchTerm] = useState("");
 
-    )
- }
+  const handleSearch = (searchTerm) => {
+    dispatch(searchCountry(searchTerm));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    handleSearch(searchTerm); // Dispatch the searchCountry action with the searchTerm
+    setSearchTerm("");
+  };
+
+  const onChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter any country name"
+        value={searchTerm}
+        onChange={onChange}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
