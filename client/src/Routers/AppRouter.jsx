f5d7797a7060f7646a1c67f83d18e8c60da0ee6d
nicolaspratg <nicoprat@hotmail.com>
2024-02-23 13:23:21 -0300

    added /form &/activities and removed nav

--- client/src/Routers/AppRouter.jsx
+++ client/src/Routers/AppRouter.jsx
@@ -1,21 +1,19 @@
 import { Route, Routes, useLocation } from "react-router-dom";
 import Landing from "../views/Landing/Landing";
 import Home from "../views/Home/Home";
-import { Nav } from "../components/Nav/Navbar";
 import Detail from "../views/Detail/Detail";
+import Form from "../views/Form/Form";
+import Activities from "../views/Activities/Activities";
 
 export const AppRouter = () => {
-  const location = useLocation();
-  const isLanding = location.pathname === "/";
   return (
     <div className="App">
-      {!isLanding && <Nav />}
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/home" element={<Home />} />
         <Route path="/detail/:id" element={<Detail />} />
-        {/* <Route path="/form" element={<Form />} /> */}
-        {/* <Route path="/activities" element={<Activities />} /> */}
+        <Route path="/activities" element={<Activities />} />
+        <Route path="/form" element={<Form />} />
       </Routes>
     </div>
   );
