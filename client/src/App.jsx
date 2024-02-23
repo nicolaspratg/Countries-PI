7a6fad60e77fa727c488595d27e6f5a4496bd8ef
nicolaspratg <nicoprat@hotmail.com>
2024-02-23 13:40:35 -0300

    app.css and fixed imports of AppRouter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AppRouter } from "./Routers/appRouter";
+import { AppRouter } from "./Routers/AppRouter";
 import "./App.css"
 
 function App() {
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { AppRouter } from "./Routers/appRouter.jsx";
+import { AppRouter } from "./Routers/AppRouter.jsx";
 import store from "./redux/store.js";
 import { Provider } from "react-redux";
+import styles from "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-    <BrowserRouter>
-      <AppRouter />
-    </BrowserRouter>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
