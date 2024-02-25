67ab8c934e7fa8f70915a7121debb17b6a434db9
nicolaspratg <nicoprat@hotmail.com>
2024-02-25 20:33:44 -0300

    now nav is everywhere instead of just at home

--- client/src/Routers/AppRouter.jsx
+++ client/src/Routers/AppRouter.jsx
@@ -1,15 +1,21 @@
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import Landing from "../views/Landing/Landing";
 import Home from "../views/Home/Home";
 import Detail from "../views/Detail/Detail";
 import Form from "../views/Form/Form";
 import Activities from "../views/Activities/Activities";
+import { Nav } from "../components/Nav/Navbar";
+import About from "../views/About/About";
 
 export const AppRouter = () => {
+  const location = useLocation();
+  const isLanding = location.pathname === "/";
   return (
     <div className="App">
+      {!isLanding && <Nav />}
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/about" element={<About />} />
         <Route path="/home" element={<Home />} />
         <Route path="/detail/:id" element={<Detail />} />
         <Route path="/activities" element={<Activities />} />
