76aa899e46f9b6de7812d592959b13ba42a665eb
nicolaspratg <nicoprat@hotmail.com>
2024-02-25 20:32:02 -0300

    Form created and styled

--- client/src/views/Form/Form.jsx
+++ client/src/views/Form/Form.jsx
@@ -1,18 +1,9 @@
-import { NavLink } from "react-router-dom";
 import ActivityForm from "../../components/Activity Form/Activity Form";
-
+import styles from "./Form.module.css"
 const Form = () => {
   return (
-    <div>
-      <h1>Create Tourist Activity</h1>
+    <div className={styles.container}>
       <ActivityForm />
-      <h3>OR</h3>
-      <NavLink to="/activities">
-        <button>See created activities</button>
-      </NavLink>
-      <NavLink to="/home">
-        <button>Home</button>
-      </NavLink>
     </div>
   );
 };
